90dc62d3d2d3324f2dca29b2f641f971b79d3e2f
noobcoder123456789 <152793384+noobcoder123456789@users.noreply.github.com>
2024-04-16 07:03:45 +0700

    Add files via upload

---
+++ ossim_mm_tlb_hk241/src/cpu-tlb.c
@@ -0,0 +1,144 @@
+/*
+ * Copyright (C) 2024 pdnguyen of the HCMC University of Technology
+ */
+/*
+ * Source Code License Grant: Authors hereby grants to Licensee 
+ * a personal to use and modify the Licensed Source Code for 
+ * the sole purpose of studying during attending the course CO2018.
+ */
+//#ifdef CPU_TLB
+/*
+ * CPU TLB
+ * TLB module cpu/cpu-tlb.c
+ */
+ 
+#include "mm.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int tlb_change_all_page_tables_of(struct pcb_t *proc,  struct memphy_struct * mp)
+{
+  /* TODO update all page table directory info 
+   *      in flush or wipe TLB (if needed)
+   */
+
+  return 0;
+}
+
+int tlb_flush_tlb_of(struct pcb_t *proc, struct memphy_struct * mp)
+{
+  /* TODO flush tlb cached*/
+
+  return 0;
+}
+
+/*tlballoc - CPU TLB-based allocate a region memory
+ *@proc:  Process executing the instruction
+ *@size: allocated size 
+ *@reg_index: memory region ID (used to identify variable in symbole table)
+ */
+int tlballoc(struct pcb_t *proc, uint32_t size, uint32_t reg_index)
+{
+  int addr, val;
+
+  /* By default using vmaid = 0 */
+  val = __alloc(proc, 0, reg_index, size, &addr);
+
+  /* TODO update TLB CACHED frame num of the new allocated page(s)*/
+  /* by using tlb_cache_read()/tlb_cache_write()*/
+
+  return val;
+}
+
+/*pgfree - CPU TLB-based free a region memory
+ *@proc: Process executing the instruction
+ *@size: allocated size 
+ *@reg_index: memory region ID (used to identify variable in symbole table)
+ */
+int tlbfree_data(struct pcb_t *proc, uint32_t reg_index)
+{
+  __free(proc, 0, reg_index);
+
+  /* TODO update TLB CACHED frame num of freed page(s)*/
+  /* by using tlb_cache_read()/tlb_cache_write()*/
+
+  return 0;
+}
+
+
+/*tlbread - CPU TLB-based read a region memory
+ *@proc: Process executing the instruction
+ *@source: index of source register
+ *@offset: source address = [source] + [offset]
+ *@destination: destination storage
+ */
+int tlbread(struct pcb_t * proc, uint32_t source,
+            uint32_t offset, 	uint32_t destination) 
+{
+  BYTE data, frmnum = -1;
+	
+  /* TODO retrieve TLB CACHED frame num of accessing page(s)*/
+  /* by using tlb_cache_read()/tlb_cache_write()*/
+  /* frmnum is return value of tlb_cache_read/write value*/
+	
+#ifdef IODUMP
+  if (frmnum >= 0)
+    printf("TLB hit at read region=%d offset=%d\n", 
+	         source, offset);
+  else 
+    printf("TLB miss at read region=%d offset=%d\n", 
+	         source, offset);
+#ifdef PAGETBL_DUMP
+  print_pgtbl(proc, 0, -1); //print max TBL
+#endif
+  MEMPHY_dump(proc->mram);
+#endif
+
+  int val = __read(proc, 0, source, offset, &data);
+
+  destination = (uint32_t) data;
+
+  /* TODO update TLB CACHED with frame num of recent accessing page(s)*/
+  /* by using tlb_cache_read()/tlb_cache_write()*/
+
+  return val;
+}
+
+/*tlbwrite - CPU TLB-based write a region memory
+ *@proc: Process executing the instruction
+ *@data: data to be wrttien into memory
+ *@destination: index of destination register
+ *@offset: destination address = [destination] + [offset]
+ */
+int tlbwrite(struct pcb_t * proc, BYTE data,
+             uint32_t destination, uint32_t offset)
+{
+  int val;
+  BYTE frmnum = -1;
+
+  /* TODO retrieve TLB CACHED frame num of accessing page(s))*/
+  /* by using tlb_cache_read()/tlb_cache_write()
+  frmnum is return value of tlb_cache_read/write value*/
+
+#ifdef IODUMP
+  if (frmnum >= 0)
+    printf("TLB hit at write region=%d offset=%d value=%d\n",
+	          destination, offset, data);
+	else
+    printf("TLB miss at write region=%d offset=%d value=%d\n",
+            destination, offset, data);
+#ifdef PAGETBL_DUMP
+  print_pgtbl(proc, 0, -1); //print max TBL
+#endif
+  MEMPHY_dump(proc->mram);
+#endif
+
+  val = __write(proc, 0, destination, offset, data);
+
+  /* TODO update TLB CACHED with frame num of recent accessing page(s)*/
+  /* by using tlb_cache_read()/tlb_cache_write()*/
+
+  return val;
+}
+
+//#endif
