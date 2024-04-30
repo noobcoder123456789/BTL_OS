da696c89b33c6dfc726ee54c1d2187b29e902181
KietoKun <167785969+KietoKun@users.noreply.github.com>
2024-04-30 12:26:08 +0700

    Update queue.c

--- ossim_mm_tlb_hk241/src/queue.c
+++ ossim_mm_tlb_hk241/src/queue.c
@@ -9,12 +9,32 @@ int empty(struct queue_t * q) {
 
 void enqueue(struct queue_t * q, struct pcb_t * proc) {
         /* TODO: put a new process to queue [q] */
+        if (q->size < MAX_QUEUE_SIZE){
+                q->proc[q->size] = proc;
+                q->size++;
+        }
 }
 
 struct pcb_t * dequeue(struct queue_t * q) {
         /* TODO: return a pcb whose prioprity is the highest
          * in the queue [q] and remember to remove it from q
          * */
-	return NULL;
+        if (q->size == 0){
+                return NULL;
+        }
+        int max_priority = q->proc[0]->prio
+        int max_idx = 0;
+        for (int i = 0; i < q->size; i++){
+                if (q->proc[i] < q->size; i++){
+                        max_priority = q->proc[i] -> prio;
+                        max_idx = 1;
+                }
+        }
+        struct pcb_t* temp_proc = q->proc[max_idx];
+        for (int i = max_idx; i < q->size - 1; i++){
+                q->proc[i] = q->proc [i+1];
+        }
+        q->proc[q->size - 1] = NULL;
+        q->size--;
+        return temp_proc; 
 }
-
